125e4aee6fba8c7a8ebcfc43bfeda278fb128f54
Kennedy <60758839+kennedy354@users.noreply.github.com>
2022-06-18 13:45:25 -0300

    Add files via upload

---
+++ spring_deletar/src/main/java/br/com/ifce/service/JogoService.java
@@ -0,0 +1,34 @@
+package br.com.ifce.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.ifce.model.Jogo;
+import br.com.ifce.repository.JogoRepository;
+
+@Service
+public class JogoService {
+	
+	@Autowired
+	private JogoRepository jogoRepo;
+	
+	public void cadastrarJogo(Jogo jogo) {
+		jogoRepo.save(jogo);
+	}
+	
+	public List<Jogo> listarJogos() {
+		return jogoRepo.findAll();
+	}
+	
+	public void excluir(Long codigo) {
+		jogoRepo.deleteById(codigo);
+	}
+	
+	public Optional<Jogo> buscarPorID(Long codigo) {
+		return jogoRepo.findById(codigo);
+		
+	}
+}
